4ab79e42c7c4644b10f99af8165daca48272194a
Ivan Jurin <nirujnavi@gmail.com>
2015-04-06 00:59:43 +0200

    added custom shell plugins

--- shell/.zshrc
+++ shell/.zshrc
@@ -1,1 +1,10 @@
+ZSH=$HOME/.dotfiles/shell/oh-my-zsh
+ZSH_THEME="bureau"
+DISABLE_AUTO_UPDATE="true"
+DISABLE_LS_COLORS="true"
+
+plugins=(git sudo sublime fasd zsh-syntax-highlighting alias-tips)
+
+source $ZSH/oh-my-zsh.sh
+
 source ~/.bash_profile
--- shell/install.sh
+++ shell/install.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+# some deps
+sudo apt-get install git zsh cmake build-essential
+
+# install oh my zsh
+export ZSH=~/.dotfiles/shell/oh-my-zsh 
+
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install syntax highlighter
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+# install alias-tips
+git clone https://github.com/djui/alias-tips.git $ZSH/custom/plugins/zsh-syntax-highlighting
+
+
+# paste files
+
 CUR_DIR=$(pwd)
 
 FILES=(
@@ -13,3 +29,15 @@ for f in "${FILES[@]}"; do
 	test -f "~/$f" && test ! -L "~/$f" && mv -v ~/$f ~/$f.old
 	ln -sfv $CUR_DIR/$f ~/
 done
+
+# if not zsh prompt switch to zsh
+if [ ! -n "$ZSH_VERSION" ]; then
+	while true; do
+	    read -p "Do you wish to switch to zsh? (y|n)" yn
+	    case $yn in
+	        [Yy]* ) chsh -s /bin/zsh; break;;
+	        [Nn]* ) exit;;
+	        * ) echo "Please answer yes or no.";;
+	    esac
+	done
+fi
